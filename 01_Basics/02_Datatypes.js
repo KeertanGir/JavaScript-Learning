2ce9bd206fd83ffb1d7a25c643c6d95bfa45546b
Keertan Gir <gkeertangir@gmail.com>
2025-08-07 17:33:44 +0000

    learn Datatypes in javaScript

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,28 @@
+
+"use StrictMode"; // treat all js code in newer version
+
+
+
+// alert(hello)
+// alert(3 + 3) // we are using node Js not browser
+
+console.log(3 + 3); 
+console.log("keertan gir")
+
+let name = "Keertan gir" // String 
+let age = 21 // int/ Number
+let isLoggedIn = true // Boolean
+
+
+// number 0 1 to poer 53
+// bigint
+// string => ""
+// boolean => true
+// null => standalone value
+// undefiend =>
+// symbol => unique
+
+// Object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
