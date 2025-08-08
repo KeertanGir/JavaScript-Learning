a2968bc5b8d31c3e5079875b8866d2c92f7c11a4
Keertan Gir <gkeertangir@gmail.com>
2025-08-08 21:47:46 +0000

    Learn Type Convertion On Datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,32 @@
+let score = "22";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let newNumber = Number(score);
+
+console.log(typeof newNumber)
+
+
+// "33" => 33
+//  "33abc" = NaN
+//  true => 1; false => 0
+
+
+let isLogedIn = 1
+
+// console.log(typeof isLogedIn);
+
+let booleanLogedIn = Boolean(isLogedIn);
+
+// console.log(typeof booleanLogedIn);
+
+
+let num = 12
+
+console.log(typeof num)
+
+let strNum = String(num)
+
+console.log(strNum)
+console.log(typeof strNum)
