8edf2cd0c403ac62ab8ac2f1ace07f7047186d2b
Keertan Gir <gkeertangir@gmail.com>
2025-09-26 17:44:26 +0000

    Learn Some Types of Comparasion in JavaScript

---
+++ 01_Basics/04_Comparasion.js
@@ -0,0 +1,23 @@
+console.log( 12 == 12 ); // true
+console.log( "12" > 8); // true
+console.log( "1" >= 12 ); // false
+console.log( "1" < 12); // true
+console.log( "12" <= "12"); // true
+
+// null Comparasion
+// you have to avoid these Types Of Comparasions in Javacript
+console.log( null > 0 ); // false
+console.log( null  == 0); // false
+console.log( null >= 0 ); // true
+
+// Undefined
+console.log(undefined > 0); // false 
+console.log( undefined == 0 ); // false
+console.log( undefined >= 0 ); // false
+
+
+
+
+
+
+
