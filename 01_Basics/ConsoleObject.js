9c1329308e01f7b651c4717164c01dc6f3c57b0f
Keertan Gir <gkeertangir@gmail.com>
2025-10-22 03:20:43 +0500

    Add files via upload
    Learn Some Methods Of Console

---
+++ 01_Basics/ConsoleObject.js
@@ -0,0 +1,6 @@
+// In This Code im doing Practice Of Java Script Console Class
+
+console.log("Web Start");
+console.error("Network error");
+console.warn("Check your Internet");
+console.info("This is a learner Web Page");
